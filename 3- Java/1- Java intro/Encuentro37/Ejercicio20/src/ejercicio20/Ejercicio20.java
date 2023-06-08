31eac38c49435c9b563c2485c6a0802b2d208208
Angelina Balatorre <angelinabalatorre@gmail.com>
2023-06-08 20:12:39 -0300

    Correcciones del Ejercicio12

--- 3- Java/1- Java intro/Encuentro37/Ejercicio20/src/ejercicio20/Ejercicio20.java
+++ 3- Java/1- Java intro/Encuentro37/Ejercicio20/src/ejercicio20/Ejercicio20.java
@@ -8,21 +8,21 @@ public class Ejercicio20 {
         Scanner leer = new Scanner(System.in);
         //Determina el tamano de la matriz
         int n = 3;
-        int[][] matriz = { 
-            { 2, 7, 6 }, 
-            { 9, 5, 1 }, 
-            { 4, 3, 8 } 
+        int[][] matriz = {
+            {2, 7, 6},
+            {9, 5, 1},
+            {4, 3, 8}
         };
         //Mostrar la matriz por pantalla
         mostrarMatriz(matriz, n);
         //Evaluar filas, columnas y diagonales
-        boolean filas = filas(matriz,n);
-        boolean columnas = columnas(matriz,n);
-        boolean diagonalPrin = diagonalPrincipal(matriz,n);
-        boolean diagonalSec = diagonalSecundaria(matriz,n);
-        if ((filas == true) && (columnas  == true) && (diagonalPrin == true) && (diagonalSec == true)){
+        boolean filas = filas(matriz, n);
+        boolean columnas = columnas(matriz, n);
+        boolean diagonalPrin = diagonalPrincipal(matriz, n);
+        boolean diagonalSec = diagonalSecundaria(matriz, n);
+        if ((filas == true) && (columnas == true) && (diagonalPrin == true) && (diagonalSec == true)) {
             System.out.println("La matriz es magica!! :D");
-        }else{
+        } else {
             System.out.println("La matriz no es magica :(");
         }
 
@@ -89,7 +89,7 @@ public class Ejercicio20 {
         int aux = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ((i == j)){
+                if ((i == j)) {
                     aux = aux + matriz[i][j];
                 }
             }
@@ -101,22 +101,21 @@ public class Ejercicio20 {
 
         return igualdad;
     }
-    
+
     public static boolean diagonalSecundaria(int[][] matriz, int n) {
         boolean igualdad = false;
         int aux = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (((i + j) == (n - 1))){
+                if (((i + j) == (n - 1))) {
                     aux = aux + matriz[i][j];
                 }
             }
-            
-            if (aux == sumaValores(matriz, n)) {
-                igualdad = true;
-            }
         }
         
+        if (aux == sumaValores(matriz, n)) {
+            igualdad = true;
+        }
         return igualdad;
     }
 }
