96c0477c90b145809c584d3c44f342f59aa27fb5
Angelina Balatorre <angelinabalatorre@gmail.com>
2023-06-21 21:55:21 -0300

    Actividad grupal 1

---
+++ 3- Java/2- POO/Encuentro43/ActividadExtra2/src/entidades/Cuadrado.java
@@ -0,0 +1,27 @@
+package entidades;
+
+public class Cuadrado {
+    
+    private int lado;
+
+    public Cuadrado() {
+    }
+
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    public int getlado() {
+        return lado;
+    }
+
+    public void setlado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadrado{" + "lado=" + lado + '}';
+    }
+    
+}
